07ee938aa6425e02a5258fa1ad1ea2ccc16c7569
josep.fullana <josep.fullana@auleseebe.upc.edu>
2024-09-12 12:05:01 +0200

    por terminar

--- Problema3.R
+++ Problema3.R
@@ -1,1 +1,13 @@
 #Codigo para problema 3
+  #a
+x <- c(-8,5,2,-8,9,5,-3,1,-1,4,-4,9,3,-9,7,0,-7,8,-4,1,7,-6,4,5,
+       -9,-2,-8,5,-5)
+
+sum(x) 
+  #b
+#para eliminar posiciones, restamos la c
+y <- x[-c(21,2,26)]
+  #c
+sum(exp(x))-sum(exp(y))
+  #d
+y[c(19,24)] #hacerlo en consola
