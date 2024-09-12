e44a2ba725a83861beee354666cbee808a7a28d5
josep.fullana <josep.fullana@auleseebe.upc.edu>
2024-09-12 11:55:42 +0200

    Ej 2

--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,9 @@
 #Codigo para problema 2
+  #para borrar todos los datos en data
+rm(list=ls())
+
+data <- mtcars #asignamos datos
+data #ejecutamos las dos lineas para abrir los datos
+
+data$mpg
+hist(data$mpg)
